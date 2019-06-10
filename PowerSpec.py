5196c8f52a4870c3dcfe1587f14e24c50b62a876
Adekunle Ajayi <adeajayi.kunle@gmail.com>
2019-06-10 20:55:14 +0200

    PowerSpec_v2.0

--- PowerSpec.py
+++ PowerSpec.py
@@ -1,15 +1,3 @@
-
-############################################################################
-# This scripts estimate wavenumber spectrum of two dimensional oceanic dataset such as SSH, vorticity. Prior to
-# spectrum estimate, the dataset is interpolated and any NaN value is replaced with interpolated value. A
-# tapering is applied to the dataset, afterwhich, the dataset is detrend in both direction. The 2D spectral
-# obtained after FFT is radially averaged to a 1D spectral.
-#
-# Author : Adekunle Ajayi and Julien Lesommer
-# Affilation : Institut des Geosciences de l'Environnement (IGE),
-#              Universite Grenoble Alpes, France.
-# Email : adekunle.ajayi@univ-grenoble-alpes.fr, julien.lesommer@univ-grenoble-alpes.fr,
-############################################################################
 ## load modules
 import numpy as np
 import pandas as pd
@@ -19,7 +7,7 @@ import numpy.linalg as li
 import scipy.signal as signal
 from scipy.interpolate import interp1d
 
-def e1e2(navlon,navlat):
+def _e1e2(navlon,navlat):
     """Compute scale factors from navlon,navlat.
         """
     earthrad = 6371229     # mean earth radius (m)
@@ -31,8 +19,8 @@ def e1e2(navlon,navlat):
     e1 = earthrad * deg2rad * np.sqrt( (dilam * np.cos(deg2rad*phi))**2. + diphi**2.)
     e2 = earthrad * deg2rad * np.sqrt( (djlam * np.cos(deg2rad*phi))**2. + djphi**2.)
     return e1,e2
-#########################
 
+#########################
 def interpolate(data,navlon,navlat,interp=None):
     """
         Perform a spatial interpolation if required; return x_reg,y_reg,data_reg.
@@ -42,7 +30,7 @@ def interpolate(data,navlon,navlat,interp=None):
         interp : if None return data with cordinates in meters, if 'basemap', return interpolated
         data using basemap from mpl_toolkits and also cordinates in meters.
         """
-    e1,e2 = e1e2(navlon,navlat)
+    e1,e2 = _e1e2(navlon,navlat)
     x1d_in = e1[0,:].cumsum() - e1[0,0]
     y1d_in = e2[:,0].cumsum() - e2[0,0]
     x2d_in,y2d_in = np.meshgrid(x1d_in,y1d_in)
@@ -57,28 +45,13 @@ def interpolate(data,navlon,navlat,interp=None):
         data_reg=basemap.interp(data,x1d_in,y1d_in,x2d_reg,y2d_reg,checkbounds=False,order=1)
         return x2d_reg,y2d_reg,data_reg
     else: raise ValueError('Your choice of interp is not available in this sript.')
-#########################
 
-def detrend_by_fiting2dplane(x, y, data):
-    import numpy.linalg as li
-    mask = ma.getmask(data)
-    ind = ma.where(mask == 0)
-    B = data[ind]
-    X = x[ind]
-    Y = y[ind]
-    n = np.size(B)
-    A = np.vstack([X, Y, np.ones(n)]).T
-    # Solve the least square system
-    fit = li.lstsq(A, B)
-    a, b, c = fit[0]
-    data_dtr =  data - (a * x + b * y + c)
-    return data_dtr
 #########################
-
 def isdata_contain_nan(data):
     '''
         This function check if a data contains any NaN value
-        If yes, it replaces the NaN values with an interpolated value using the fill_nan function.
+        If yes, it replaces the NaN values with an interpolated
+        value using the fill_nan function.
         '''
     i_mask = data.mask
     arr = np.array(data)
@@ -89,10 +62,14 @@ def isdata_contain_nan(data):
         return data_new
     else :
         return data
-#########################
 
+#########################
 def fill_nan(data):
-    ''' replaces a NaN value in a dataset with an interpolated one'''
+    ''' replaces a NaN value in a dataset with an interpolated one.
+        
+        return :
+        data_nonan
+        '''
     # - Reshape to 1D array
     i,j = data.shape
     _1D_arr = data.reshape(i*j,1)
@@ -102,68 +79,71 @@ def fill_nan(data):
     # - get array length
     indice = np.arange(len(_1D_arr))
     # - interpolate for nan value only
-    interp = interp1d(indice[nt_nan], _1D_arr[nt_nan],kind ='linear')
+    interp = interp1d(indice[nt_nan], _1D_arr[nt_nan],kind ='linear',fill_value="extrapolate")
     # - replace nan values
     arr = interp(indice)
     # - reshape back to 2D array
     data_nonan = arr.reshape(i,j)
     return data_nonan
-#########################
 
-def tukey(Ni,Nj):
+#########################
+def _tukey(Ni,Nj):
     ''' Using tukey window : tapered cosine window. /alpha = 0.5'''
-    wdwi =  signal.tukey(Ni,0.5)
-    wdwj =  signal.tukey(Nj,0.5)
+    wdwi =  signal.tukey(Ni,0.5,sym=False)
+    wdwj =  signal.tukey(Nj,0.5,sym=False)
     wdw = wdwi[np.newaxis,...]*wdwj[...,np.newaxis]
     return wdw
-#########################
 
-def hanning(Ni,Nj):
+#########################
+def _hanning(Ni,Nj):
     ''' Using Hanning window'''
-    wdwi =  signal.hanning(Ni)
-    wdwj =  signal.hanning(Nj)
+    wdwi =  signal.hanning(Ni,sym=False)
+    wdwj =  signal.hanning(Nj,sym=False)
     wdw = wdwi[np.newaxis,...]*wdwj[...,np.newaxis]
     return wdw
-#########################
 
-def wavenumber_vector(Ni,Nj,dx,dy):
+#########################
+def _wavenumber_vector(x,y,Ni,Nj):
     ''' Compute a wavenumber vector  '''
-    kx = np.fft.fftshift(np.fft.fftfreq(Ni,dx)) # two sided
-    ky = np.fft.fftshift(np.fft.fftfreq(Nj,dy))
+    # - obtain dx and dy
+    x1dreg,y1dreg = x[0,:],y[:,0]
+    dx=np.int(np.ceil(x1dreg[1]-x1dreg[0]))
+    dy=np.int(np.ceil(y1dreg[1]-y1dreg[0]))
+    
+    kx = np.fft.fftfreq(Ni,dx) # two sided
+    ky = np.fft.fftfreq(Nj,dy)
+    Kmax = max(kx.max(), ky.max())
     
     k, l = np.meshgrid(kx,ky)
-    kradial = np.sqrt(k**2 + l**2)
-    kmax = min(k.max(),l.max())
-    
-    dkx = np.abs(kx[2]-kx[1])
-    dky = np.abs(ky[2]-ky[1])
-    dkradial = np.sqrt(dkx**2 + dky**2)
+    wavnum2D = np.sqrt(k**2 + l**2)
     
     # radial wavenumber
-    wavnum = dkradial*np.arange(1,int(kmax/dkradial))
-    return wavnum,kradial
-#########################
+    ddk = 1./(dx*Ni)
+    ddl = 1./(dy*Nj)
+    
+    dK = max(ddk,ddl)
+    wavnum1D = dK*np.arange(1,int(Kmax/dK))
+    return wavnum1D,wavnum2D,kx,ky,dx,dy
 
-def get_spec_2D(data_reg,dx,dy,Ni,Nj):
+#########################
+def _get_2D_psd(data_reg,dx,dy,Ni,Nj):
     ''' Compute the 2D spectrum of the data '''
     spec_fft = np.fft.fft2(data_reg)
     spec_2D = (spec_fft*spec_fft.conj()).real*(dx*dy)/(Ni*Nj)
-    spec_2D = np.fft.fftshift(spec_2D)
     return spec_2D
-#########################
 
-def get_spec_1D(kradial,wavnum,spec_2D):
+#########################
+def _get_1D_psd(kradial,wavnum,spec_2D):
     ''' Compute the azimuthaly avearge of the 2D spectrum '''
     spec_1D = np.zeros(len(wavnum))
     for i in range(wavnum.size):
         kfilt =  (kradial>=wavnum[i] - wavnum[0]) & (kradial<=wavnum[i])
         N = kfilt.sum()
-        spec_1D[i] = (spec_2D[kfilt].sum())*wavnum[i]/N  #
-    #spec_1D[i] = ((2.0*np.pi)*spec_2D[kfilt]*(kradial[kfilt])).sum()/N
+        spec_1D[i] = (spec_2D[kfilt].sum())*wavnum[i]/N
     return spec_1D
-#########################
 
-def detrend_data(x,y,data,detrend):
+#########################
+def _detrend_data(x,y,data,detrend):
     if detrend is None :
         data = data
         return data
@@ -171,9 +151,6 @@ def detrend_data(x,y,data,detrend):
         # - detrend data in both direction
         data = signal.detrend(data,axis=0,type='linear')
         data = signal.detrend(data,axis=1,type='linear')
-        return data
-    elif detrend == 'FitPlane':
-        data = detrend_by_fiting2dplane(x, y, data)
         return data
     elif detrend == 'Zonal':
         # - detrend data in the zonal direction
@@ -190,57 +167,164 @@ def detrend_data(x,y,data,detrend):
         data = signal.detrend(data,axis=1,type='linear')
         return data
     else: raise ValueError('Your choice of detrend is not available in this sript.')
-#########################
 
-def apply_windowing(Ni,Nj,data,window):
+#########################
+def _apply_windowing(data,window):
+    Nj,Ni = data.shape
     if window is None:
         data = data
         return data
     elif window == 'Hanning':
-        wdw = hanning(Ni,Nj)
+        wdw = _hanning(Ni,Nj)
         data*=wdw
         return data
     elif window == 'Tukey':
-        wdw = tukey(Ni,Nj)
+        wdw = _tukey(Ni,Nj)
         data*=wdw
         return data
     else: raise ValueError('Your choice of windowing is not available in this sript.')
+
 #########################
+def preprocess_data(data,detrend,window,navlon,navlat):
+    x,y,data = interpolate(data,navlon,navlat)  # get x and y cordinates
+    data = isdata_contain_nan(data)             # check if data contains NaN value
+    data = _detrend_data(x,y,data,detrend)      # Detrend
+    data = _apply_windowing(data,window)        # Apply windowing
+    return data,x,y
 
-def get_spectrum(data,x,y,window='Tukey',detrend='Both'):
-    """
-        data_reg : Interpolated data.
-        x_reg and y_reg : interpolate coordinates in meters.
-        window : None , 'Hanning' or 'Tukey' (tappered consine window with /apha = 0.5).
-        detrend :
-        if "both" : detrend the 2D data along both axes.
-        if "zonal" : detrend the data in the zonal direction only
-        if "RemoveMean" : Remove only the mean of the data
-        if 'RmeanDtrend' : Remove the mean then detrend the data in both direction
-        if None : use the raw data
+#########################
+def _get_transfer1D(wavnum2D,wavnum1D,spec_2D):
+    ''' Compute KE Transfer'''
+    transfer = np.zeros(len(wavnum1D))
+    for i in range(wavnum1D.size):
+        kfilt =  ((wavnum1D[i] - wavnum1D[0]) <= wavnum2D) & (wavnum2D <= wavnum1D[i])
+        transfer[i] = (spec_2D[kfilt]).sum()
+    return transfer
+
+#########################
+def _get_grad_uv_in_fourier_space(u,v,kx,ky,Ni,Nj):
+    ''' gradient of u and v in spectral space
+        return dudx,dudy,dvdx,dvdy
+        '''
+    dudx = np.real(np.fft.ifft2(np.fft.fft2(u)*(1j*kx*2*np.pi)[None, :]));
+    dudy = np.real(np.fft.ifft2(np.fft.fft2(u)*(1j*ky*2*np.pi)[:, None]));
+    dvdx = np.real(np.fft.ifft2(np.fft.fft2(v)*(1j*kx*2*np.pi)[None, :]));
+    dvdy = np.real(np.fft.ifft2(np.fft.fft2(v)*(1j*ky*2*np.pi)[:, None]));
+    return dudx,dudy,dvdx,dvdy
+
+#########################
+def wavenumber_spectra(data,navlon,navlat,window='Tukey',detrend='Both'):
+    """ This function computes wavenumber spectral density of a two dimensional dataset.
+        
+        Input
+            data : Two dimensional dataset Eg SSH
+            navlon : Longitude
+            navlat : Latitude
+            window : None , 'Hanning' or 'Tukey' : (tappered consine window with /apha = 0.5).
+            detrend :
+                "both" : detrend the 2D data along both axes.
+                "zonal" : detrend the data in the zonal direction only
+                "RemoveMean" : Remove only the mean of the data
+                "RmeanDtrend" : Remove the mean then detrend the data in both direction
+                None : use the raw data
+        Return
+            wavenumber : Horizontal wavenumber
+            psd : isotropic power spectral density
         """
-    # Get data shape
-    Nj,Ni = data.shape
-    
-    # Detrend
-    detrended_data = detrend_data(x,y,data,detrend)
-    
-    # Obtain dx and dy
-    x1,y1 = x[0,:],y[:,0]
-    dx=np.int(np.ceil(x1[1]-x1[0]))
-    dy=np.int(np.ceil(y1[1]-y1[0]))
-    
-    # wavenumber vector
-    wavnum,kradial = wavenumber_vector(Ni,Nj,dx,dy)
-    
-    # Apply windowing
-    windowed_data = apply_windowing(Ni,Nj,detrended_data,window)
-    
-    # Estimate 2D spectra
-    spec_2D = get_spec_2D(windowed_data,dx,dy,Ni,Nj)
-    
-    # Average 2D spectra to 1D spectra
-    spec_1D = get_spec_1D(kradial,wavnum,spec_2D)
-    psd = spec_1D
-    return wavnum,psd
+    Nj,Ni = data.shape                                                      # Get data shape
+    data,x,y = preprocess_data(data,detrend,window,navlon,navlat)           # Remove NaN, Detrend, apply windowing
+    wavenumber,wavenumber_2D,kx,ky,dx,dy = _wavenumber_vector(x,y,Ni,Nj)    # Get wavenumber
+    spec_2D = _get_2D_psd(data,dx,dy,Ni,Nj)                                 # Estimate 2D spectra
+    psd = _get_1D_psd(wavenumber_2D,wavenumber,spec_2D) # Average 2D spectra to 1D spectra : isotropic wavenumber spectra density
+    return wavenumber,psd
 
+
+#########################
+def spectra_flux(_u,_v,navlon,navlat,window='Tukey',detrend='Both'):
+    '''This fuctions implement the computation of kinetic energy spectral flux.
+        flux = \int^{ks}_{k} u ....
+        
+        Input
+            _u : Two dimensional zonal velocity dataset.
+            _v : Two dimensional meriodinal velocity dataset.
+            navlon : Longitude
+            navlat : Latitude
+            window : None , 'Hanning' or 'Tukey' : (tappered consine window with /apha = 0.5).
+            detrend :
+                "both" : detrend the 2D data along both axes.
+                "zonal" : detrend the data in the zonal direction only
+                "RemoveMean" : Remove only the mean of the data
+                "RmeanDtrend" : Remove the mean then detrend the data in both direction
+                None : use the raw data
+        Return
+            wavenumber : Horizontal wavenumber
+            flux : kinetic energy spectral flux
+        '''
+    Nj,Ni = _u.shape                                             # Get data shape
+    u,x,y = preprocess_data(_u,detrend,window,navlon,navlat)     # Remove NaN, Detrend, apply windowing
+    v,x,y = preprocess_data(_v,detrend,window,navlon,navlat)     # Remove NaN, Detrend, apply windowing
+    wavenumber,wavenumber_2D,kx,ky,dx,dy = _wavenumber_vector(x,y,Ni,Nj)
+    dudx,dudy,dvdx,dvdy = _get_grad_uv_in_fourier_space(u,v,kx,ky,Ni,Nj) # Get gradient of u and v
+    # - compute terms
+    phi1 = u*dudx + v*dudy
+    phi2 = u*dvdx + v*dvdy
+    # - compute FFT
+    tm1 = (fft.fft2(u).conj())*fft.fft2(phi1)
+    tm2 = (fft.fft2(v).conj())*fft.fft2(phi2)
+    
+    transfer_2D = -1*np.real(tm1 + tm2)/np.square(Ni*Nj) # - KE transfer
+    transfer_1D = _get_transfer1D(wavenumber_2D,wavenumber,transfer_2D) # Get 1D transfer : assume isotropy
+    flux = np.cumsum(transfer_1D[::-1])[::-1] # - Get flux
+    return wavenumber,flux
+
+
+def cross_spectra(data1,data2,navlon,navlat,window='Tukey',detrend='Both',Normalize=True):
+    '''This fuctions compute the spectra coherence between two 2D datasets
+        
+        Input
+            _u : Two dimensional zonal velocity dataset.
+            _v : Two dimensional meriodinal velocity dataset.
+            navlon : Longitude
+            navlat : Latitude
+            window : None , 'Hanning' or 'Tukey' : (tappered consine window with /apha = 0.5).
+            detrend :
+                "both" : detrend the 2D data along both axes.
+                "zonal" : detrend the data in the zonal direction only
+                "RemoveMean" : Remove only the mean of the data
+                "RmeanDtrend" : Remove the mean then detrend the data in both direction
+                None : use the raw data
+        Return
+            wavenumber : Horizontal wavenumber
+            cross spectra : spectra coherence between data1 and data2
+        '''
+    Nj,Ni = data1.shape                                                 # Get data shape
+    data1,x,y = preprocess_data(data1,detrend,window,navlon,navlat)     # Remove NaN, Detrend, apply windowing
+    data2,x,y = preprocess_data(data2,detrend,window,navlon,navlat)     # Remove NaN, Detrend, apply windowing
+    wavenumber,wavenumber_2D,kx,ky,dx,dy = _wavenumber_vector(x,y,Ni,Nj)        # wavenumber vector
+    spec_2D = (np.fft.fft2(data1) * np.fft.fft2(data2).conj()).real*(dx*dy)/(Ni*Nj) # Cross spectra
+    
+    cross_spectra = _get_1D_psd(wavenumber_2D,wavenumber,spec_2D) # Average 2D spectra to 1D spectra
+    
+    if Normalize:
+        cs1_2D = _get_2D_psd(data1,dx,dy,Ni,Nj); cs1_1D = _get_1D_psd(wavenumber_2D,wavenumber,cs1_2D)
+        cs2_2D = _get_2D_psd(data2,dx,dy,Ni,Nj); cs2_1D = _get_1D_psd(wavenumber_2D,wavenumber,cs2_2D)
+        cross_spectra = (abs(cross_spectra)**2)/(cs1_1D*cs2_1D)
+    return wavenumber,cross_spectra
+
+
+def weighted_scale(wavenumber,psd,axs=None):
+    '''Compute weighted scale from wavenumber spectra density.
+        just like integral scale
+        input :
+            wavenumber ==> wavenumber
+            psd ==> Power spectral density
+        return :
+            scale ==> weighted scale
+        '''
+    if len(psd.shape) == 2:
+        if not axs: raise ValueError('Provide axis to sum over')
+        return psd.sum(axis=axs)/(wavenumber*psd).sum(axis=axs)
+    if len(psd.shape) > 2:
+        raise ValueError('Dimension of psd is more than 2')
+    else:
+        return psd.sum()/(wavenumber*psd).sum()
